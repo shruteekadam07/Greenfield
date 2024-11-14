95c5fb77fc184dee33b34d7012a64e55485a6902
Shruti Kadam <shruti.kadam@simplifyhealthcare.com>
2024-11-14 12:21:34 +0000

    session management

---
+++ ECommerce/HDFCBankApp/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace HDFCBankApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string email,string password)
+        {
+            if (email == "shruti@gmail.com" && password == "seed")
+            {
+                this.HttpContext.Session["loggedinUser"] = email;
+                return RedirectToAction("welcome");
+            }
+
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Welcome()
+        {
+            string email = this.HttpContext.Session["loggedinUser"] as string;
+            ViewBag.Email = email;
+            return View();
+        }
+    }
+}
